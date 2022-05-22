318aa0d4ce8777de1eb76ec680300b870027554f
Samuel Aigbotsua <saigbotsua@ymail.com>
2022-05-22 18:00:54 +0100

    add web client config properties for timeouts

--- src/main/java/com/saig/drex/configuration/HttpWebClientConfig.java
+++ src/main/java/com/saig/drex/configuration/HttpWebClientConfig.java
@@ -3,6 +3,7 @@ package com.saig.drex.configuration;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.netty.http.client.HttpClient;
@@ -13,13 +14,22 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class HttpWebClientConfig {
 
+    @Value("${http.web.client.timeout.connect:5000}")
+    private int connectTimeout;
+    @Value("${http.web.client.timeout.read:5000}")
+    private int readTimeout;
+    @Value("${http.web.client.timeout.write:5000}")
+    private int writeTimeout;
+    @Value("${http.web.client.timeout.response:5000}")
+    private int responseTimeout;
+
     @Bean
     public HttpClient httpClient() {
         return HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-                .responseTimeout(Duration.ofMillis(5000))
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeout)
+                .responseTimeout(Duration.ofMillis(responseTimeout))
                 .doOnConnected(connection -> connection
-                        .addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-                        .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
+                        .addHandlerLast(new ReadTimeoutHandler(readTimeout, TimeUnit.MILLISECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(writeTimeout, TimeUnit.MILLISECONDS)));
     }
 }
